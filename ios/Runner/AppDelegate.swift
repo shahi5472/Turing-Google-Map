b9f9c0cfa8da1c9c234f12c2c6093f3149974e73
S.M. SHAHi <s.m.kamalhussain@gmail.com>
2023-01-28 18:23:50 +0600

    ios location permission enable

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCONLzKdcEAm5KZBaOKFxMS3L4VGb27wGo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
